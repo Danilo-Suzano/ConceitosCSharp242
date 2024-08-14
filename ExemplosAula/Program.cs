ece6f3de7953417bcc46b434bd4dd890fe3acb52
202211154 <202211154@TPCP04LAB0505>
2024-08-14 20:00:28 -0300

    Adicionar arquivos de projeto.

---
+++ ExemplosAula/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+ * comentário em bloco
+ */
+
+Console.WriteLine("Informe seu nome");
+string nome = Console.ReadLine();
+Console.WriteLine($"Seja bem vindo(a) {nome}");
+Console.WriteLine("Informe sua idade");
+int idade = int.Parse(Console.ReadLine());
+//De acordo coma idade informada diga se o usuário pode ou não votar
+
+
+if (idade >= 18 && idade < 65)
+{ 
+    Console.WriteLine("Deve votar");
+} 
+else if (idade>= 16  || idade >= 65)
+{
+    Console.WriteLine("Pode votar");
+}
+else 
+{
+    Console.WriteLine("Não pode votar");
+}
+//modifique o código para que diferencie o Pode Votar do Deve Votar
+
+
